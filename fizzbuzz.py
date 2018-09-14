540fc9cf338446a6f9077e3022f78469eb4f49b6
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-14 12:26:47 -0400

    Updated from Brython Server: 9/14/2018 12:26:47 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -47,7 +47,7 @@ Buzz
 fizz = input("For multiples of what number shall we print 'Fizz'? ")
 buzz = input("For multiples of what number shall we print 'Buzz'? ")
 for i in range(1,100):
-    if i%int(fizz) = 0 and i%int(buzz) = 0
+    if i%int(fizz) = 0 and i%int(buzz) = 0:
         print("FizzBuzz")
     elif i%int(fizz) = 0:
         print("Fizz")
